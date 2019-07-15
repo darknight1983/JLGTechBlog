552d2409aa8cd090c122005f787389052883824d
Jacolby Green <darknightnov@gmail.com>
2019-07-15 15:04:38 -0500

    first commit

---
+++ src/main/java/com/JLGBlog/JLGTechBlog/JlgTechBlogApplication.java
@@ -0,0 +1,13 @@
+package com.JLGBlog.JLGTechBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlgTechBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlgTechBlogApplication.class, args);
+	}
+
+}
